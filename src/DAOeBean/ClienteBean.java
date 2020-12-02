caac251633208cece4a14df23a5a415e78fa7d3d
Rafael Goulart <Rafael.Goulart2@etecia.com.br>
2020-12-01 23:43:04 -0300

    2nd Commit  - Pacote DAOeBean criado    - ClienteBean % EmpresaBean criados    - Concluídos  - Pacote screens criado    - JFClientes & JFEmpresas & JFMenu criados    - em andamento

---
+++ src/DAOeBean/ClienteBean.java
@@ -0,0 +1,62 @@
+package DAOeBean;
+
+public class ClienteBean {
+    private int id;
+    private String nome;
+    private String telres;
+    private String telcom;
+    private String telcel;
+    private String email;
+          
+    public ClienteBean() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelres() {
+        return telres;
+    }
+
+    public void setTelres(String telres) {
+        this.telres = telres;
+    }
+
+    public String getTelcom() {
+        return telcom;
+    }
+
+    public void setTelcom(String telcom) {
+        this.telcom = telcom;
+    }
+
+    public String getTelcel() {
+        return telcel;
+    }
+
+    public void setTelcel(String telcel) {
+        this.telcel = telcel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/DAOeBean/EmpresaBean.java
@@ -0,0 +1,46 @@
+package DAOeBean;
+
+public class EmpresaBean {
+    private int codigoemp;
+    private String nomeemp;
+    private String cnpj;
+    private String razsocial;
+
+    public EmpresaBean() {
+        
+    }
+    
+    public int getCodigoemp() {
+        return codigoemp;
+    }
+
+    public void setCodigoemp(int codigoemp) {
+        this.codigoemp = codigoemp;
+    }
+
+    public String getNomeemp() {
+        return nomeemp;
+    }
+
+    public void setNomeemp(String nomeemp) {
+        this.nomeemp = nomeemp;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazsocial() {
+        return razsocial;
+    }
+
+    public void setRazsocial(String razsocial) {
+        this.razsocial = razsocial;
+    }
+    
+    
+}
